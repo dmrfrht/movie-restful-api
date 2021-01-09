5a68f3b684ba51d257e82347412c5c67be96802e
dmrfrht <dmrfrht1@gmail.com>
2021-01-09 21:24:07 +0300

    tum movieleri listeleyen endpoint olusturuldu

--- routes/movie.js
+++ routes/movie.js
@@ -4,8 +4,16 @@ var router = express.Router();
 // Models
 const Movie = require('../Models/Movie')
 
-/* GET movie listing. */
-router.post('/', function (req, res, next) {
+/* GET movie  */
+router.get('/', (req, res) => {
+  const promise = Movie.find({});
+  promise
+    .then(data => res.json(data))
+    .catch(err => res.json(err))
+})
+
+/* POST movie */
+router.post('/', (req, res, next) => {
   const movie = new Movie(req.body)
   const promise = movie.save()
 
