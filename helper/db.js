1a8e5998650dcf633d2f2bbd2adc4202f8e2884f
dmrfrht <dmrfrht1@gmail.com>
2021-01-09 19:39:58 +0300

    proje altyapisi express-generetor yardimi ile olusturulup mongodb atlas ile baglanti saglandi

---
+++ helper/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+module.exports = () => {
+  mongoose.connect('mongodb+srv://rootUser:135792468@cluster0.zidcw.mongodb.net/movie-app?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+  
+  mongoose.connection.on('open', () => {
+    console.log('mongodb connected')
+  })
+
+  mongoose.connection.on('error', (err) => {
+    console.log('mongodb: error', err)
+  })
+}
