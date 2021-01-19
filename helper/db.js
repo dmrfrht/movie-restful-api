778cf9bebad278f6874d764dc3e1ecd75a8e48a8
dmrfrht <dmrfrht1@gmail.com>
2021-01-19 15:48:54 +0300

    start script

--- helper/db.js
+++ helper/db.js
@@ -4,7 +4,7 @@ module.exports = () => {
   mongoose.connect('mongodb+srv://rootUser:135792468@cluster0.zidcw.mongodb.net/movie-app?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 
   mongoose.connection.on('open', () => {
-    // console.log('mongodb connected')
+    console.log('mongodb connected')
   })
 
   mongoose.connection.on('error', (err) => {
